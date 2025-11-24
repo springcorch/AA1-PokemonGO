ca4a39c5c15f408c7c54dbf8ec00c4307f67bad0
Sam <sam.corchero@estudiant.enti.cat>
2025-11-24 18:24:04 +0100

    Initial LoginScreen.kt

--- app/src/main/java/com/example/pokemongo/screens/LoginScreen.kt
+++ app/src/main/java/com/example/pokemongo/screens/LoginScreen.kt
@@ -1,9 +1,125 @@
 package com.example.pokemongo.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.pokemongo.R
 
 //IAGO HAS MADE THIS SCREEN
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
 fun LoginScreen(modifier: Modifier, navController: NavController) {
+    Column(
+        modifier
+            .fillMaxSize()
+            .background(color = MaterialTheme.colorScheme.primary),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+        Box(modifier = modifier.fillMaxWidth().padding(bottom = 34.dp),
+            contentAlignment = Alignment.BottomCenter)
+        {
+            Image(
+                painter = painterResource(id = R.drawable.loginscreenbackground),
+                contentDescription = "",
+                modifier = Modifier.alpha(0.75f)
+                    .fillMaxWidth()
+                    .padding(16.dp).padding(bottom = 16.dp)
+                    .clip(RoundedCornerShape(40.dp))
 
+            )
+            Text(
+                text = "Log In",
+                color = MaterialTheme.colorScheme.onTertiary,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.displayLarge,
+            )
+        }
+        Box(modifier = modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopCenter)
+        {
+            val mailInputExample = "Enter your mail or username"
+            Text(
+                text = "Mail or Username",
+                color = MaterialTheme.colorScheme.onTertiary,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+
+            OutlinedTextField(
+                value = mailInputExample,
+                onValueChange = {},
+                modifier = modifier
+                    .padding(16.dp).padding(top = 8.dp)
+                    .border(1.dp, Color.White, RoundedCornerShape(8.dp)),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true,
+            )
+        }
+        Spacer(modifier = Modifier.padding(horizontal = 0.dp, vertical = 16.dp))
+        Box(modifier = modifier.fillMaxWidth(),
+            contentAlignment = Alignment.TopCenter)
+        {
+            Text(
+                text = "Password",
+                color = MaterialTheme.colorScheme.onTertiary,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+            val passwordInputExample = "Enter your password"
+
+
+            OutlinedTextField(
+                value = passwordInputExample,
+                onValueChange = {},
+                modifier = modifier
+                    .padding(16.dp).padding(top = 8.dp)
+                    .border(1.dp, Color.White, RoundedCornerShape(8.dp)),
+                shape = RoundedCornerShape(8.dp),
+                singleLine = true
+            )
+        }
+        Spacer(modifier = Modifier.padding(horizontal = 0.dp, vertical = 20.dp))
+        Button(onClick = { navController.navigate("homeScreen") },
+            colors = ButtonDefaults.buttonColors(
+                containerColor = MaterialTheme.colorScheme.secondary),
+            modifier = modifier
+                .size(width = 200.dp, height = 75.dp)) {
+            Text(text = "SUBMIT",
+                fontSize = 25.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+
+        Spacer(modifier = Modifier.padding(vertical = 8.dp))
+        Button(onClick = { navController.navigate("signInScreen") }) {
+            Text(text = "Don't have an account? Sign Up!",
+                color = MaterialTheme.colorScheme.onPrimary)
+        }
+    }
 }
