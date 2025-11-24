e72e833359d6a799c2bec4f35c52bca1122a8f5b
Sam <sam.corchero@estudiant.enti.cat>
2025-11-24 17:35:43 +0100

    Initial Section.kt

--- app/src/main/java/com/example/pokemongo/data/Section.kt
+++ app/src/main/java/com/example/pokemongo/data/Section.kt
@@ -2,8 +2,20 @@ package com.example.pokemongo.data
 
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import com.example.pokemongo.R
 
 annotation class Section(
     @DrawableRes val img: Int,
     @StringRes val name: Int
 )
+
+fun loadSections(): List<Section> {
+    return listOf<Section>(
+        return listOf(
+            Section(R.drawable.section1image, R.string.section1),
+            Section(R.drawable.section2image, R.string.section2),
+            Section(R.drawable.section3image, R.string.section3),
+            Section(R.drawable.section4image, R.string.section4),
+        )
+    )
+}
